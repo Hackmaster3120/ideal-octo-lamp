05057eee6d35f115147dcb841b6a680001216e94
Hackmaster3120 <81357992+Hackmaster3120@users.noreply.github.com>
2022-03-28 16:37:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_die = loadSound("mariodie.wav");
+	Mjump = loadSound("jump.wav");
+	Mkick = loadSound("kick.wav");
+	Mcoin = loadSound("coin.wav");
+	game_over = loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas')
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+    video.size(800,400);
+	video.parent('GameCons');
+	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results){
+	if(results.length > 0){
+	  console.log(results)
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+}
+
+
+
+
+
+
